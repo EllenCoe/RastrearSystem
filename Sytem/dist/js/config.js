0dc5b7e401b8071c07a294e8f4944c4f5415ee38
Ramon Ranieri <ramonranieri@hotmail.com>
2018-12-30 13:56:05 -0300

    Ramon e Gabii

---
+++ Sytem/dist/js/config.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyApO4wXWN5EWfXJNDvroFEYhph1wuXP_Ck",
+  authDomain: "rastrearapp-f9d72.firebaseapp.com",
+  databaseURL: "https://rastrearapp-f9d72.firebaseio.com",
+  projectId: "rastrearapp-f9d72",
+  storageBucket: "rastrearapp-f9d72.appspot.com",
+  messagingSenderId: "880909374126"
+};
+firebase.initializeApp(config);
